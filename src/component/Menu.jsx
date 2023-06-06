5ba06106bff084907ade06a689d9e93f1631aed2
vijayviki2 <vijayviki07@gmail.com>
2023-06-07 00:28:37 +0530

    created menu

---
+++ src/component/Menu.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Menu() {
+    return (
+        <nav className="navbar navbar-expand-md navbar-dark bg-success">
+            <div className="container">
+            <NavLink to={`/`} className="navbar-brand">React Hooks</NavLink>
+
+            <button className="navbar-toggler" data-bs-toggler="collapse" data-bs-target="#menu"></button>
+
+            <div className="collaps navbar-collapse" id="menu">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                    <NavLink to={`/ex1`} className="nav-link">Ex1</NavLink>
+                    </li>
+                    <li className="nav-item">
+                    <NavLink to={`/ex2`} className="nav-link">Ex4</NavLink>
+                    </li>
+                    <li className="nav-item">
+                    <NavLink to={`/ex3`} className="nav-link">Ex3</NavLink>
+                    </li>
+                    <li className="nav-item">
+                    <NavLink to={`/ex4`} className="nav-link">Ex4</NavLink>
+                    </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+    )
+}
+export default Menu
